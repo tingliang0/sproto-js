35575bf6cccbb685443fde2adb4daf339bb0247a
zhangshiqian1214 <814729280@qq.com>
2018-09-20 17:13:12 +0800

    add some fix from https://github.com/KAndQ/sproto-js.git

--- utils.js
+++ utils.js
@@ -1,7 +1,6 @@
-
-var utils = (function(){
+var utils = (function() {
     var t = {};
-    
+
     t.array2arraybuffer = function(array) {
         var b = new ArrayBuffer(array.length);
         var v = new DataView(b, 0);
@@ -10,7 +9,7 @@ var utils = (function(){
         }
         return b;
     }
-    
+
     t.arraybuffer2array = function(buffer) {
         var v = new DataView(buffer, 0);
         var a = new Array();
@@ -20,9 +19,10 @@ var utils = (function(){
         return a;
     }
 
-    t.string2utf8 = function(str){
+    t.string2utf8 = function(str) {
         var back = [];
         var byteSize = 0;
+
         for (var i = 0; i < str.length; i++) {
             var code = str.charCodeAt(i);
             if (0x00 <= code && code <= 0x7f) {
@@ -39,26 +39,31 @@ var utils = (function(){
                 back.push((128 | (63 & code)))
             }
         }
+
         for (i = 0; i < back.length; i++) {
             back[i] &= 0xff;
         }
+
         return back;
     };
 
-    t.utf82string = function(arr){
+    t.utf82string = function(arr) {
         if (typeof arr === 'string') {
             return null;
         }
+
         var UTF = '';
         for (var i = 0; i < arr.length; i++) {
-            if (arr[i] == null){
+            if (arr[i] == null) {
                 break;
             }
+
             var one = arr[i].toString(2);
             var v = one.match(/^1+?(?=0)/);
             if (v && one.length == 8) {
                 var bytesLength = v[0].length;
                 var store = arr[i].toString(2).slice(7 - bytesLength);
+
                 for (var st = 1; st < bytesLength; st++) {
                     store += arr[st + i].toString(2).slice(2);
                 }
@@ -71,17 +76,21 @@ var utils = (function(){
         return UTF;
     };
 
-    t.arrayconcat = function(a1, a2){
+    t.arrayconcat = function(a1, a2) {
         var b = new Array();
-        for (var i=0; i<a1.length; i++){
+
+        for (var i = 0; i < a1.length; i++) {
             b[i] = a1[i];
         }
-        for (var j=a1.length; j<a1.length + a2.length; j++){
-            b[j] = a2[j-a1.length];
+
+        for (var j = a1.length; j < a1.length + a2.length; j++) {
+            b[j] = a2[j - a1.length];
         }
+
         return b;
     };
 
     return t;
 }());
+
 module.exports = utils;
