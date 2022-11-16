e4101edcee9665dfb29da5166afc916854bbc527
zhangshiqian <814729280@qq.com>
2022-11-16 19:47:13 +0800

    feat: update code

--- test_c2s_all.js
+++ test_c2s_all.js
@@ -25,16 +25,17 @@ console.log(sp);
 {
 
     // 1. 客户端传输数据给服务器
-    let buffer = clientSender("GetUserInfo",null, 1);
+    let buffer = clientSender("test_double_msg", {test1: 100000, test2: 100000, test3: '100000'}, 1);
+    console.log(buffer.toString());
 
     // console.log(buffer);
     // 2. 服务端接收处理数据, 再返回数据给客户端
     let ret = serverHost.dispatch(buffer);
     console.log(ret);
     console.log("==========================");
-    buffer = ret.responseFunc({userInfo : { nickname: "先知比利", id: "123", gold: 4294967, test_double : 1999999999.321 }});
+    // buffer = ret.responseFunc({userInfo : { nickname: "先知比利", id: "123", gold: 4294967, test_double : 300000 }});
 
     // 3. 客户端接收服务器的返回数据
-    ret = clientHost.dispatch(buffer);
-    console.log(ret);
+    // ret = clientHost.dispatch(buffer);
+    // console.log(ret);
 }
